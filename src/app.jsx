c54eb34762071a995c2b24f8e6d38bdc9b43a4b2
moamel-hashim <muamal1997@gmail.com>
2023-01-13 17:30:28 -0800

    adding start up files

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+
+  }
+
+  render() {
+    return (
+      <>
+      <h1>hello react</h1>
+      </>
+    )
+  }
+}
