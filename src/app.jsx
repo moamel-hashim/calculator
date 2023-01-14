d5741ce9d8c3777d3ea1c5ab6a40df8fe9fdeec4
moamel-hashim <muamal1997@gmail.com>
2023-01-13 17:51:01 -0800

    creating skeleton

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 export default class App extends React.Component {
-  constructor(props) {
-
-  }
 
   render() {
     return (
       <>
-      <h1>hello react</h1>
+      <div className="component">
+        
+      </div>
       </>
     )
   }
